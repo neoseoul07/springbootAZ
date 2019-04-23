ccff87f326305bff5d6bfaf5cbf7234ec5cdc78e
Yaswanth Tavva <yaswanth.tavva@livspace.com>
2019-04-23 16:39:42 +0530

    hibernate lost update case & some comments added

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,18 +1,26 @@
 package com.example;
 
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.apache.logging.log4j.LogManager;
 
 /**
  * Created by yaswanth on 15/03/19.
  */
 @SpringBootApplication
 public class Application {
+    private static final Logger LOGGER = LogManager.getLogger(Application.class);
+
     public static void main(String[] args) {
 //        This is to run as non-web application
 //        new SpringApplicationBuilder(Application.class).web(WebApplicationType.NONE).run(args);
         SpringApplication.run(Application.class, args);
+
+        LOGGER.info("Info level log message");
+        LOGGER.debug("Debug level log message");
+        LOGGER.error("Error level log message");
     }
 }
--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -6,6 +6,12 @@ import com.example.dao.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.LockModeType;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.Iterator;
+
 /**
  * Created by yaswanth on 15/03/19.
  */
@@ -18,7 +24,11 @@ public class MainController {
     @Autowired
     private UserDao userDao;
 
+    @PersistenceContext
+    private EntityManager em;
+
     @RequestMapping(value = "/add", method = RequestMethod.GET)
+    @Transactional
     public String addNewUser (@RequestParam String name, @RequestParam String email) {
         User n = new User();
         n.setName(name);
@@ -26,14 +36,45 @@ public class MainController {
         Address address = new Address();
         address.setAddress("testing");
         n.getAddresses().add(address);
-        userRepository.save(n);
+//        entity manager requires @Transactional whereas jpa repository doesn't need it
+//        userRepository.save(n);
+        em.persist(n);
         return "Saved";
     }
 
     @GetMapping(path="/all")
     public Iterable<User> getAllUsers() {
         // This returns a JSON or XML with the users
-//        return userDao.getAll();
         return userRepository.findAll();
+//        Hibernate maintains cache per session to reduce hitting db multiple times.
+//        write-behind cache -> flushes changes to cache only when it
+//        is required(ex. object with id param changed and fetching again in same session/transaction).
+//        Commits changes only to db in the end of transaction.
+
+//        User u = userRepository.findById(1).get();
+//        u = userRepository.findById(6).get();
+//        This won't trigger mysql query again as 6 id is fetched already
+//        u = userRepository.findById(6).get();
+//        return u;
+
+    }
+
+    @PutMapping(path="/{user_id}")
+    @Transactional
+    public User updateUser(@PathVariable(value = "user_id") int user_id) throws InterruptedException {
+//        User u = userRepository.findById(1).get();
+//        An exclusive lock blocks both shared lock & exclusive lock
+//        An shared lock blocks only exclusive lock
+        User u = em.find(User.class, 1, LockModeType.PESSIMISTIC_WRITE);
+//        System.out.println(u.getName());
+        if (user_id == 1) {
+            u.setName("test2");
+            u.setEmail("testing@gmail.com");
+        } else {
+            u.setName("test3");
+        }
+        Thread.sleep(10000);
+//        u = userRepository.findById(user_id).get();
+        return u;
     }
 }
