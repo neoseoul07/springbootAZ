4c97940fe766995456cf4ac30a5e27afe74e8ee9
Yaswanth Tavva <yaswanth.tavva@livspace.com>
2019-08-07 20:29:39 +0530

    war packaging enabled

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -6,13 +6,19 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.apache.logging.log4j.LogManager;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * Created by yaswanth on 15/03/19.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
     private static final Logger LOGGER = LogManager.getLogger(Application.class);
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
 
     public static void main(String[] args) {
 //        This is to run as non-web application
